c1d3f845a25cc15ab9a570a6f0d51a1d0e56deb1
gustavotrentinidev <mila.e.ferreira28@gmail.com>
2022-07-07 15:55:28 -0300

    commit Inicial

---
+++ core/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from core.models import Categoria
+
+admin.site.register(Categoria)
+
+from core.models import Autor
+
+admin.site.register(Autor)
+
+
---
+++ core/models.py
@@ -0,0 +1,22 @@
+from pyexpat import model
+from django.db import models
+
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.descricao
+
+class Editora(model.Model):
+    nome = models.CharField(max_length=255)
+    site = models.URLField()
+
+    def __str__(self):
+        return self.nome
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nome

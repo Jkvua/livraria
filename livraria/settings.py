0cacde4c219b882dedf92c9bbacd6bc0e65739fb
= <=>
2022-08-18 16:43:43 -0300

    allowed_hosts

--- livraria/settings.py
+++ livraria/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-$^@*9rw8+(f4fz#or+bnvbe8=gqpdao2djkp(*ruw5sfsgj2rv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['livraria-camila.herokuapp.com']
 
 
 # Application definition
